27c8b8b6072635737b3ebcb30372dd13257cb220
Tahsin Jahin <tahsinjahinkhalid@gmail.com>
2019-11-18 16:39:22 +0600

    Added scripts

---
+++ site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+      echo "<h1>My Webpage</h1>";
+      echo "<hr>";
+      echo "<p>This webpage is mine!</p>";
+
+      $characterName = "Jahin";
+      $characterAge = 22;
+
+      echo "My name is $characterName.<br>";
+      echo "My age is $characterAge.<br>"
+     ?>
+
+  </body>
+</html>
