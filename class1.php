c008808ab0667ac55ef820132a919247f6671b3d
Tahsin Jahin <tahsinjahinkhalid@gmail.com>
2019-11-19 19:45:32 +0600

    added the notes from the first class

---
+++ class1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Knyght learns php</title>
+  </head>
+  <body>
+
+  <?php include 'header.html'?>
+
+  <?php
+    // echo "Hello World! <br>";
+
+    // print "Hello World Again! <br>";
+
+    // variables
+    $title = "Php Web Frameworks";
+    $num1 = 10;
+    $num2 = 30;
+
+    // var_dump($num2);
+
+    // echo $title;
+    // echo $num1 + $num2;
+
+    // can type HTML again after PHP
+    // block is eliminated
+
+    // array
+    $arrayName = array('a', 'b', 'c', 'd');
+
+    var_dump($arrayName);
+  ?>
+
+  <?php include 'footer.html'?>
+
+  </body>
+</html>
